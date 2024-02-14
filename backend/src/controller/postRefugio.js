640f7d0c3abd8598839fed374b95ec0da334074a
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-02-14 19:34:37 -0300

    modelo refugio y post para prueba

---
+++ backend/src/controller/postRefugio.js
@@ -0,0 +1,36 @@
+//funcion de prueba para el posteo de refugios
+
+import Refugio from "../models/refugios.js";
+
+const postRefugio = async (req, res) => {
+  try {
+    const { name, userName, password, phone, email, adress, website, instagram, description, image } = req.body; // Extrae los datos del cuerpo de la solicitud
+
+    const existingRefugio = await Refugio.findOne({ email });
+    if (existingRefugio) {
+      return res.status(400).json({ message: "Ya existe un refugio con este correo electrónico" });
+    }
+
+    const refugio = new Refugio({
+      name,
+      userName,
+      password,
+      phone,
+      email,
+      adress,
+      website,
+      instagram,
+      description,
+      image,
+    });
+
+    await refugio.save();
+
+    // Devuelve una respuesta de éxito
+    res.status(201).json({ message: "Refugio creado exitosamente", refugio });
+  } catch (error) {
+    console.error("Error al crear el refugio:", error);
+    res.status(500).json({ message: "Hubo un problema al procesar la solicitud" });
+  }
+};
+export default postRefugio;
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,4 +1,8 @@
-import express from "express"
-const router = express.Router()
+import express from "express";
 
-export default router
+const router = express.Router();
+import postRefugio from "../controller/postRefugio.js";
+
+router.post("/refugio", postRefugio);
+
+export default router;
