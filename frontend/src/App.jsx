cffbe278950125aa44d7032922ee3a42ad6a2598
Edwin Amaranto Villegas <63481204+EdwinCoder85@users.noreply.github.com>
2024-02-14 01:19:29 -0500

    se agrego el link de styles.css en head del index.html

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="bg-red-400 rounded-lg">
-      <h2 className="text-xl">APP</h2>
+      <h2 className="text-xl font-bold p-4 text-white">APP</h2>
     </div>
   )
 }
