c82ed8a3405d47d2b2fe5ee88b1d38f7a912b6be
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-02-14 22:33:51 -0300

    get de refugios

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import postRefugio from "../controller/postRefugio.js";
+import getRefugio from "../controller/getRefugio.js";
 
 const router = express.Router();
-import postRefugio from "../controller/postRefugio.js";
 
 router.post("/refugio", postRefugio);
+router.get("/refugio", getRefugio);
 
 export default router;
