2e29dbf3d814a100bd6b21dc1f293e45b916028e
Gabriel <loveragabriel20@gmail.com>
2024-02-14 22:25:18 -0300

    se crea models Pet con Gonzalo y Matias Marensi

---
+++ backend/src/models/pets.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import User from "./usuarios"
+
+const petsSchema = new mongoose.Schema({
+    pet_type: {
+        type:String, 
+        required: true,
+        enum:['Perro','Gato'], 
+    },
+    name: {
+        type:String, 
+        required: true,
+        trim: true,
+        maxlength: 20, 
+        validate: {
+            validator: (value) => /^[a-zA-Z]+$/.test(value),
+            message: 'El nombre solo debería ser letras'
+          }
+    }, 
+    age: {
+        type:Number, 
+        required: true, 
+        min: 0, 
+        validate: {
+            validator: (value) => value >= 0,
+            message: `Debes ingresar un valor igual o mayor a 0`
+          }
+    },
+    gender:{
+        type:String, 
+        required: true,
+        enum:['Macho', 'Hembra']
+    }, 
+    characteristics: {
+        type: String, 
+        required: true, 
+        enum:['Calmado','Jugueton','Alegre','Tranquilo','Cariñoso', 'Guardian']
+    }, 
+    // id_fundation:{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Refugio'
+    // }
+    status: {
+        type:Boolean, 
+        required: true, 
+        default:true
+    },
+    // adoption_data:{
+    //     type:, 
+    //     required: true, 
+    //     default: true
+    // }, 
+      adopter:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', 
+        default:null
+    }
+})
+
+export default mongoose.model("Pet", userSchema);
