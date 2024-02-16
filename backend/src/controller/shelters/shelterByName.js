eda8735f4fab118a3bc135b20a7d4d091302b1dc
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-02-16 18:17:48 -0300

    prueba!

--- backend/src/controller/shelters/shelterByName.js
+++ backend/src/controller/shelters/shelterByName.js
@@ -10,6 +10,7 @@ const shelterByName = async (req, res) => {
   } catch (error) {
     console.error("Error al buscar refugios por nombre:", error);
     return res.status(500).json({ message: "Hubo un problema al procesar la solicitud" });
+    //
   }
 };
 
