6aac788cb1f210fb236e28cd1834cd9c5f00af82
Gabriel <loveragabriel20@gmail.com>
2024-02-19 00:40:28 -0300

    creo file pet.controller.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,9 +4,12 @@ const router = express.Router()
 
 import postPet from "../controller/pets/potsPets.js";
 import getPets from "../controller/pets/getPets.js";
+import getPetsId from " "
 
 router.post("/pet", postPet);
 router.get("/pet", getPets);
+router.get("/pet/:id", getPetsId);
+
 
 
 export default router;
