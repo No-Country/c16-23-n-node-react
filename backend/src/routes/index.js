82002a897cb98d70686851919d69f7241d64e941
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-02-20 22:11:11 -0300

    correcciones menores, eliminacion de ruta.post de shelter que no se usaba

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,16 +1,12 @@
-
 import express from "express";
 
-import postPet from "../controller/pets/postPets.js";
-import getPets from "../controller/pets/getPets.js";
 import shelterRegister from "../controller/register/shelterRegister.js";
 import shelterRoutes from "./shelterRoutes.js";
-import petRoutes from "./petRoutes.js";
+
 import verified from "../controller/register/verified.js";
-import petRoute from "./pet.routes.js"
-import postShelter from "../controller/postShelter.js";
+import petRoute from "./pet.routes.js";
+
 import userRoute from "./user.routes.js";
-import getShelter from "../controller/getShelter.js";
 
 const router = express.Router();
 
@@ -19,18 +15,6 @@ router.use("/shelter", shelterRoutes);
 
 router.put("/verify-email", verified);
 router.post("/register", shelterRegister);
-router.use('/pet', petRoute)
-
-
-
-
-
-
-
-
-
-
-
+router.use("/pet", petRoute);
 
 export default router;
-
