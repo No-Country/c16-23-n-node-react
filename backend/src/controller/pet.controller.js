3600ef9f9f3f97922624ef8af91b440be49f48d2
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-02-22 22:09:58 -0300

    estructura de funciones y login de shelter

--- backend/src/controller/pet.controller.js
+++ backend/src/controller/pet.controller.js
@@ -1,26 +1,30 @@
 import petService from "../services/pet.service.js";
 import fs from "fs-extra";
-import {uploadImage, deleteImage} from "../helpers/cloudinary.js"; 
+import { uploadImage, deleteImage } from "../helpers/cloudinary.js";
 
 const petController = {
   createPet: async (req, res) => {
     try {
       const data = req.body;
-      if(!Array.isArray(req.files?.image)){
-        req.files.image = [req.files.image];
-      }
-      if (req.files?.image){
-        const imagesToUpload = [];
-        for (const image of req.files.image) {
-          const result = await uploadImage(image.tempFilePath); 
-          fs.unlink(image.tempFilePath)
-          const folder = result.public_id; 
-          const url = result.secure_url; 
-          const imgs = {folder, url};
-          imagesToUpload.push(imgs); 
+      if (req.files && req.files.image) {
+        if (!Array.isArray(req.files?.image)) {
+          req.files.image = [req.files.image];
         }
-        data.images = imagesToUpload; 
-    }
+        if (req.files?.image) {
+          const imagesToUpload = [];
+          for (const image of req.files.image) {
+            const result = await uploadImage(image.tempFilePath);
+            fs.unlink(image.tempFilePath);
+            const folder = result.public_id;
+            const url = result.secure_url;
+            const imgs = { folder, url };
+            imagesToUpload.push(imgs);
+          }
+          data.images = imagesToUpload;
+          console.log(imagesToUpload);
+        }
+      }
+
       const pet = await petService.createPet(data);
       return res.status(201).json(pet);
     } catch (error) {
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,8 @@
 import express from "express";
 
-import shelterRegister from "../controller/register/shelterRegister.js";
-import shelterRoutes from "./shelterRoutes.js";
-
 import verified from "../controller/register/verified.js";
 import petRoute from "./pet.routes.js";
-
+import shelterRoutes from "./shelter.routes.js";
 import userRoute from "./user.routes.js";
 
 const router = express.Router();
@@ -14,7 +11,7 @@ router.use("/user", userRoute);
 router.use("/shelter", shelterRoutes);
 
 router.put("/verify-email", verified);
-router.post("/register", shelterRegister);
+
 router.use("/pet", petRoute);
 
 export default router;
