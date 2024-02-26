84825f217c96aa825374133fb8134bd37209a51d
karelis <karelis.montenegrom@gmail.com>
2024-02-26 19:41:01 -0300

    "Cambios3"

--- frontend/src/components/ChangePassword1.jsx
+++ frontend/src/components/ChangePassword1.jsx
@@ -1,11 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+
+let counter= 0;
 
 function ChangePassword1 () {
 
-  // const [oldPassword, setOldPassword] = useState(''); 
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [oldPassword, setOldPassword] = useForm(''); 
+  const [newPassword, setNewPassword] = useForm('');
+  const [confirmPassword, setConfirmPassword] = useForm('');
+  const [message, setMessge] = useState('');
   // const [imagen, setImagen] = useState("");
 
   const handleSubmit = (e) => { 
@@ -30,17 +34,9 @@ function ChangePassword1 () {
     } 
     }; 
 
-  // const handleImageUpload = (event) => { //para la imagen
-  //   const selectedFile = event.target.files[0];
-    
-  //   if (selectedFile) {
-      
-  //     setImagen(URL.createObjectURL(selectedFile));
-  //   }
-  // };
-
 
   return (
+    <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
     <main className="font-poppins bg-Primary text-black select-none">
       <section className="min-h-screen flex justify-center items-center gap-4 p-3">
       <div className=" block text-2xl font-bold leading-normal mb-10 text-center gap-4 p-3">
@@ -48,26 +44,18 @@ function ChangePassword1 () {
             <img className="rounder-full" src={imagen} alt="Imagen relacionada al cambio de contraseña" /> 
           )}
 
+
       <h2 className=" ml-2 text-sm rounder-full gap-4 p-3">Contraseña Actual</h2>
       <input 
-        type="password"
-        placeholder="Contraseña Actual"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
+          {...oldPassword("")} placeholder="Old password"
       />
       <h2 className=" ml-2 text-sm rounder-full gap-4 p-3">Contraseña Nueva</h2>
       <input 
-        type="password"
-        placeholder="Contraseña Nueva"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
+         {...newPassword("")} placeholder="New password"
       />
       <h2 className=" ml-2 text-sm rounder-full gap-4 p-3">Repetir Contraseña</h2>
       <input 
-        type="password"
-        placeholder="Confirmar Contraseña"
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
+         {...confirmPassword("")} placeholder="Confirm password"
       />
      
       <button className="bg-Positive text-white-500 font-poppins p-3 w-64 my-4 rounded-full relative " onClick={handleChangePassword1}>Actualizar Contraseña</button>
@@ -77,6 +65,7 @@ function ChangePassword1 () {
       </section>
             
       </main>
+      </form>
   );
 }
 
--- frontend/src/components/InformationProfi.jsx
+++ frontend/src/components/InformationProfi.jsx
@@ -1,79 +1,19 @@
 import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+let counter= 0;
 
 function InformationProfi () {
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [message, setMessage] = useState('');
-  const [imagen, setImagen] = useState("");
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
 
-  const handleInformation= (e) => {
-    const { name, value } = e.target;
-    if (name === 'name') {
-      setName(value);
-    } else if (name === 'email') {
-      setEmail(value);
-    } else if (name === 'phoneNumber') {
-      setPhoneNumber(value);
-    } 
-
-  };
-
-    
-    const handleInformationP = () => {
-      const errors = {}; 
-  
-      if (!name.trim()) {
-        errors.name = "Nombre es obligatorio";
-      }
-  
-      if (!email.trim()) {
-        errors.email = "Correo electrónico es obligatorio";
-      } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-        errors.email = "Correo electrónico inválido";
-      }
-
-      
-    if (Object.keys(errors).length > 0) {
-      setErrors(errors);
-      return;
-    }
-
-    // Implementar una mayor validación y lógica aquí (por ejemplo, comparación de contraseñas)
-
-    setMessage("Datos guardados exitosamente");
-
-    // Limpiar errores y entradas después de un envío exitoso
-    setErrors({});
-    setName("");
-    setEmail("");
-    setPhoneNumber("");
-  };
-    
-
-        if (name === email === phoneNumber ) {
-          setError(true) 
-          setMessage('Datos Correctos');
-          return
-    
-        } else {
-          setMessage('Datos Incorrectos');
-        }
-      };
-    
-      const handleImageUpload = (event) => {
-        const selectedFile = event.target.files[0];
-        
-        if (selectedFile) {
-          
-          setImagen(URL.createObjectURL(selectedFile));
-      };
+  return (
+    <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
     
     
-      return (
-        <main className="font-poppins bg-Primary text-black select-none">
-          <section className="min-h-screen flex justify-center items-center gap-4 p-3">
+      <main className="font-poppins bg-Primary text-black select-none">
+        <section className="min-h-screen flex justify-center items-center gap-4 p-3">
           <div className=" block text-2xl font-bold leading-normal mb-10 text-center gap-4 p-3">
           {imagen && (
                 <img className="rounder-full" src={imagen} alt="Imagen relacionada al cambio de contraseña" /> 
@@ -81,24 +21,16 @@ function InformationProfi () {
     
           <h2 className=" ml-2 text-sm rounder-full gap-4 p-3">Nombre</h2>
           <input 
-            type="name"
-            placeholder="Nombe y Apellido"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+             {...register("firstName")} placeholder="First name" 
           />
+        
           <h2 className=" ml-2 text-sm rounder-full gap-4 p-3">Correo Electrónico</h2>
           <input 
-            type="email"
-            placeholder="Correo Electrónico"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            {...register("email")} placeholder="Email"
           />
           <h2 className=" ml-2 text-sm rounder-full gap-4 p-3">Número de Teléfono</h2>
           <input 
-            type="phoneNumber"
-            placeholder="Número de Teléfono"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
+            {...register("phoneNumber")} placeholder="Phone Number"
           />
          
           <button className="bg-Positive text-white-500 font-poppins p-3 w-64 my-4 rounded-full relative " onClick={handleInformationP}>Actualizar Datos</button>
@@ -108,6 +40,8 @@ function InformationProfi () {
           </section>
                 
           </main>
+   </form>
+
       );
     }
     
