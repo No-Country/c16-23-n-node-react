a762ad09849a498e4b191c3b12cd579133a5288a
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-03-05 22:01:43 -0300

    mercadopago

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ import verified from "../controller/register/verified.js";
 import petRoute from "./pet.routes.js";
 import shelterRoutes from "./shelter.routes.js";
 import userRoute from "./user.routes.js";
+import { donations } from "../controller/mercadopago.js";
 
 const router = express.Router();
 
@@ -11,7 +12,7 @@ router.use("/user", userRoute);
 router.use("/shelter", shelterRoutes);
 
 router.get("/verify-email", verified);
-
+router.post("/mercadopago", donations);
 router.use("/pet", petRoute);
 
 export default router;
