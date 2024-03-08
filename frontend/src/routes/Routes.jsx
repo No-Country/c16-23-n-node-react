25f1646894606b1a366ad79355dc797496780d0e
Maximiliano Marigno <marignomaxy@gmail.com>
2024-03-07 22:46:38 -0300

    se saca de protegida dos rutas

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -2,7 +2,7 @@
 import { Route, Routes } from "react-router-dom";
 import { useLocalStorage } from "react-use";
 import { UserProvider } from "../context/UserProvider";
-import {PetProvider} from "../context/PetContext"
+import { PetProvider } from "../context/PetContext";
 import HomePage from "../pages/HomePage/HomePage";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import LogoutPage from "../pages/LoginPage/LogoutPage";
@@ -29,44 +29,50 @@ function Rutas() {
   return (
     <UserProvider>
       <PetProvider>
-      <Routes>
-        <Route path="/loginUser" element={<LoginPage />} />
-        <Route path="/loginShelter" element={<LoginPage />} />
-        <Route path="/loginTypes" element={<TypeRegister />} />
-        <Route path="/adopterRegistration" element={<RegisterAdopterPage />} />
-        <Route path="/shelterRegistration" element={<RegisterShelterPage />} />
-        <Route path="/petInformation/:id" element={<PetInformationPage />} />
-        <Route path="/logout" element={<LogoutPage />} />
+        <Routes>
+          <Route path="/loginUser" element={<LoginPage />} />
+          <Route path="/loginShelter" element={<LoginPage />} />
+          <Route path="/loginTypes" element={<TypeRegister />} />
+          <Route
+            path="/adopterRegistration"
+            element={<RegisterAdopterPage />}
+          />
+          <Route
+            path="/shelterRegistration"
+            element={<RegisterShelterPage />}
+          />
+          <Route path="/petInformation/:id" element={<PetInformationPage />} />
+          <Route path="/logout" element={<LogoutPage />} />
 
-        <Route
-          path="/shelterInformation/:id"
-          element={<ShelterInformationPage />}
-        />
-        <Route
-          element={
-            <ProtectedRoutes canActivate={user} redirectPath="/loginUser" />
-          }
-        >
-        <Route path="/petdashboard" element={<PetDashboard />} />
-        <Route path="/newpet" element={<NewPet />} />
-        <Route path="/adopcionForm" element={<AdopcionFormPage />} />
-        </Route>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/aboutus" element={<AboutUsPage />} />
-        <Route path="/shelters" element={<SheltersPage />} />
-        <Route path="*" element={<NotFound404Page />} />
-        <Route path="/userprofile" element={<UserProfilePage />} />
-        <Route path="/shelterprofile" element={<ShelterProfilePage />} />
-        <Route
-          path="/updateUserInformation"
-          element={<UpdateUserInformationPage />}
-        />
-        <Route
-          path="/updateUserPassword"
-          element={<UpdateUserPasswordPage />}
-        />
-        <Route path="/adopcionForm/:id" element={<AdopcionFormPage />} />
-      </Routes>
+          <Route
+            path="/shelterInformation/:id"
+            element={<ShelterInformationPage />}
+          />
+          <Route
+            element={
+              <ProtectedRoutes canActivate={user} redirectPath="/loginUser" />
+            }
+          >
+            <Route path="/adopcionForm" element={<AdopcionFormPage />} />
+          </Route>
+          <Route path="/petdashboard" element={<PetDashboard />} />
+          <Route path="/newpet" element={<NewPet />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/aboutus" element={<AboutUsPage />} />
+          <Route path="/shelters" element={<SheltersPage />} />
+          <Route path="*" element={<NotFound404Page />} />
+          <Route path="/userprofile" element={<UserProfilePage />} />
+          <Route path="/shelterprofile" element={<ShelterProfilePage />} />
+          <Route
+            path="/updateUserInformation"
+            element={<UpdateUserInformationPage />}
+          />
+          <Route
+            path="/updateUserPassword"
+            element={<UpdateUserPasswordPage />}
+          />
+          <Route path="/adopcionForm/:id" element={<AdopcionFormPage />} />
+        </Routes>
       </PetProvider>
     </UserProvider>
   );
