36fb5339283fdeb00b4ad9717424ee612e2a1614
EdwinCoder85 <edwinagemiler17@gmail.com>
2024-03-13 19:32:33 -0500

    se crearon las funciones asincronas deletePet y updatePet, y se retiraron los mensajes de consol.log innecesarios

--- frontend/src/hooks/usePets.js
+++ frontend/src/hooks/usePets.js
@@ -3,24 +3,21 @@ import { useState } from "react";
 
 const usePets = () => {
   const defaultBaseUrl = "https://c16-23-n-node-react-production.up.railway.app/api";
-
   const [petInfo, setPetInfo] = useState([]);
 
-  const getPetInfo = (path = "/pet") => {
-    const url = `${defaultBaseUrl}${path}`;
+  async function getPetInfo() {
+    const url = `${defaultBaseUrl}/pet/`;
     axios
       .get(url)
       .then((res) => {
         const pets = res.data.filter((pet) => pet.adoption_status === true);
         setPetInfo(pets);
-        // console.log("Response from API:", res.data);
         setPetInfo(res.data);
-
       })
       .catch((err) => console.error("Error pet info:", err));
-  };
+  }
 
-  const getPetInfoById = async (id) => {
+  async function getPetInfoById(id) {
     const url = `${defaultBaseUrl}/pet/${id}`;
     try {
       const response = await axios.get(url);
@@ -30,9 +27,9 @@ const usePets = () => {
       console.error("Error pet info by ID:", error);
       throw error;
     }
-  };
+  }
 
-  const updateAdoptionStatus = async (id, currentStatus) => {
+  async function updateAdoptionStatus(id, currentStatus) {
     const url = `${defaultBaseUrl}/pet/${id}`;
     try {
       const response = await axios.get(url);
@@ -40,32 +37,76 @@ const usePets = () => {
       pet.adoption_status = !currentStatus;
       await axios.put(url, pet);
     } catch (error) {
-      console.error("Error actualizando estado de adopción:", error);
+      console.error("Error updating adoption status:", error);
     }
-  };
-  const createPet = async (petData) => {
+  }
+  async function createPet(petData) {
     const url = `${defaultBaseUrl}/pet`;
     try {
       const formData = new FormData();
+
       Object.entries(petData).forEach(([key, value]) => {
         formData.append(key, value);
       });
-      petData.images.forEach((image) => {
-        formData.append("image", image);
-      });
+
+      console.log(petData.images);
+      if (petData.images && Array.isArray(petData.images)) {
+        petData.images.forEach((image) => {
+          console.log(image);
+          formData.append("image", image);
+        });
+      }
+      console.log(formData);
       const response = await axios.post(url, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
       });
-      // alert("Mascota creada correctamente!");
       return response.data;
     } catch (error) {
       console.error("Error creando mascota:", error);
       throw error;
     }
+  }
+
+  async function deletePet(id) {
+    const url = `${defaultBaseUrl}/pet/${id}`;
+    try {
+      axios.delete(url).then((res) => {
+        console.log(res.data);
+        const infoApiFiltered = petInfo.filter((ele) => ele.id !== id);
+        setPetInfo(infoApiFiltered);
+      });
+    } catch (error) {
+      console.error("Error deleting pet:", error);
+    }
+  }
+
+  async function updatePet(id, data) {
+    const url = `${defaultBaseUrl}/pet/${id}`;
+    try {
+      console.log(data);
+      const res = await axios.put(url, data);
+      const updatedPet = res.data;
+      const updatedPetInfo = petInfo.map((pet) =>
+        pet.id === id ? updatedPet : pet,
+      );
+      setPetInfo(updatedPetInfo);
+    } catch (error) {
+      console.error("Error updating pet:", error);
+      throw error;
+    }
+  }
+
+  return {
+    petInfo,
+    getPetInfo,
+    getPetInfoById,
+    updateAdoptionStatus,
+    createPet,
+    deletePet,
+    updatePet,
   };
-  return { petInfo, getPetInfo, getPetInfoById, updateAdoptionStatus, createPet };
 };
 
 export default usePets;
